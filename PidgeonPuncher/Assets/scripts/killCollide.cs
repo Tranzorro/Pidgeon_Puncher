bf375b34b0d85f2dee4d72f2dd7b04c30d8e14fe
Tranzorro <shiekadam@gmail.com>
2018-05-23 08:26:19 -0400

    initial commit Pidgeon Puncher
    the pidgeoning for the punchening.

---
+++ PidgeonPuncher/Assets/scripts/killCollide.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class killCollide : MonoBehaviour {
+
+    void OnCollisionEnter(Collision col)
+    {
+        //check collision name
+        Debug.Log("collision name = " + col.gameObject.name);
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("de-rezzing self");
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ PidgeonPuncher/Assets/scripts/pigeonSpawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pigeonSpawn : MonoBehaviour
+{
+
+    public float spawnTime = 5f;
+    //The amount of time between each spawn.
+    public float spawnDelay = 3f;
+    //The amount of time before spawning starts.
+    public GameObject[] pigeon;
+    //Array of enemy prefabs.
+    public Vector3 enposition;
+
+    void Start()
+    {
+        //Start calling the Spawn function repeatedly after a delay.
+        InvokeRepeating("Spawn", spawnDelay, spawnTime);
+    }
+
+    void Spawn()
+    {
+        //Instantiate a random enemy.
+        int enemyIndex = Random.Range(0, pigeon.Length);
+        Instantiate(pigeon[enemyIndex], enposition, transform.rotation);
+    }
+}
