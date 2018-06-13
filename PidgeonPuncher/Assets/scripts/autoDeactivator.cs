d5839189fc081bfa330a85faf71afb32e2f68508
Tranzorro <shiekadam@gmail.com>
2018-06-13 19:07:10 -0400

    HAS SOUND
    fixed poo pngs to fully load, instantiate on collide, fade to destroy on invisible.
    poop spawns at random locations on the screen.
    cycles between 4 poop images at a time.
    added sound effects for poop and splat.
    TODO: add punch smack sound, add pigeon coo sound, add variations of each to cycle through. add a score retention system (for every 20 pigeons you punch, gain 1 coin) add option to add weapons to hand (extend reach, ability to block poo)

---
+++ PidgeonPuncher/Assets/scripts/autoDeactivator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class autoDeactivator : MonoBehaviour {
+
+    public Image image;
+
+    private void Update()
+    {
+        //image.GetComponent<CanvasRenderer>().SetAlpha(0.1f);
+        image.CrossFadeAlpha(0f, 1f, false);
+    }
+
+    void OnBecomeInvisible()
+    {
+        this.gameObject.SetActive(false);
+        Destroy(this.gameObject);
+    }
+}
--- PidgeonPuncher/Assets/scripts/playerHealth.cs
+++ PidgeonPuncher/Assets/scripts/playerHealth.cs
@@ -6,11 +6,22 @@ using UnityEngine.UI;
 
 public class playerHealth : MonoBehaviour {
 
+    public AudioClip splat;
+    private AudioSource source;
     public float CurrentHealth;
     public float MaxHealth;
+    public GameObject[] Poo;
+    GameObject currentpoo;
+    public RectTransform Gui;
+    int index;
 
+    
     public  Slider Healthbar;
 
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
 
     public void ChangeHealth(float amount)
     {
@@ -19,21 +30,42 @@ public class playerHealth : MonoBehaviour {
 
         // Healthbar.value = CurrentHealth / MaxHealth;
         Healthbar.value = CurrentHealth;
-    }
+    } 
 
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "poop")
             ChangeHealth(-1f);
+            Debug.Log("gross, you got pooped on!");
+            RandoPoo();
+            Debug.Log("poop smeared away, but you feel gross.");
+            source.PlayOneShot(splat, 1F);
         if (CurrentHealth <= 0f)
             Die();
     }
-
+    
     void Die()
     {
         //CurrentHealth = 0;
         Debug.Log("Grossed out by pigeons, Game Over.");
         SceneManager.LoadScene("GameOverMenu", LoadSceneMode.Single);
+    }
+
+    void RandoPoo()
+    {
+        
+        index = Random.Range(0, Poo.Length);
+        currentpoo = Poo[index];
+        Vector3 spawnPosition = GetBotomLeftCorner(Gui) - new Vector3(Random.Range(0, Gui.rect.x), Random.Range(0, Gui.rect.y), 0);
+        Instantiate(currentpoo, spawnPosition, Quaternion.identity, Gui);
+        //newPoo.SetActive(true);  //don't use this here, it makes it lag for some reason, not sure why.
+    }
+
+    Vector3 GetBotomLeftCorner(RectTransform rt)
+    {
+        Vector3[] v = new Vector3[4];
+        rt.GetWorldCorners(v);
+        return v[0];
     }
 }
